02dc97d3c7e2e9151bd08a0b64ab12c5585637a1
orcemaceski <orce@maceski.net>
2022-09-13 14:26:12 +0200

    Added from address from sendgrid verified senders

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,5 +1,5 @@
 class UserNotifierMailer < ApplicationMailer
-  default :from => 'orce@kennedytech.mk'
+  default :from => 'maceskiorce@gmail.com'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_email
