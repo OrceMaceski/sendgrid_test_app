4563ed7b02dba6ba8adeefa056983a002f29c50f
orcemaceski <orce@maceski.net>
2022-09-13 14:20:19 +0200

    production values

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: 587,
-  domain: 'localhost:3000',
+  domain: ENV['DOMAIN_URL'],
   user_name: ENV['SENDGRID_USERNAME'],
   password: ENV['SENDGRID_API_KEY'],
   authentication: :plain,
